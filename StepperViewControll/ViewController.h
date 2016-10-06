b06a46110ee9e0be44ef346cc76e764459b42071
Averett Paul <averettpaulb@gmail.com>
2016-10-06 13:15:24 -0500

    Initial Commit

---
+++ StepperViewControll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StepperViewControll
+//
+//  Created by AVERETT BARKSDALE on 10/6/16.
+//  Copyright © 2016 AVERETT BARKSDALE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StepperViewControllTests/StepperViewControllTests.m
@@ -0,0 +1,39 @@
+//
+//  StepperViewControllTests.m
+//  StepperViewControllTests
+//
+//  Created by AVERETT BARKSDALE on 10/6/16.
+//  Copyright © 2016 AVERETT BARKSDALE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StepperViewControllTests : XCTestCase
+
+@end
+
+@implementation StepperViewControllTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
